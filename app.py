81cb181330f18066886429f69a530c0759a9e8b1
clouther <mc.mathieu.cloutier@gmail.com>
2020-12-29 20:40:15 -0500

    adding

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+from src.business_logic.process_query import create_business_logic
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    return f'Hello dear students, you should use an other route:!\nEX: get_stock_val/<ticker>\n'
+
+
+@app.route('/get_stock_val/<ticker>', methods=['GET'])
+def get_stock_value(ticker):
+    bl = create_business_logic()
+    prediction = bl.do_predictions_for(ticker)
+
+    return f'{prediction}\n'
+
+
+if __name__ == '__main__':
+    # Used when running locally only. When deploying to Cloud Run,
+    # a webserver process such as Gunicorn will serve the app.
+    app.run(host='localhost', port=8080, debug=True)
