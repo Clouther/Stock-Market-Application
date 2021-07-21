2f82fd16cf9592c93e2350bab746e9e824fd7807
Clouther <38193183+Clouther@users.noreply.github.com>
2021-07-20 23:36:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -68,5 +68,3 @@ if __name__ == '__main__':
     # Used when running locally only. When deploying to Cloud Run,
     # a webserver process such as Gunicorn will serve the app.
     app.run(host='localhost', port=8080, debug=True)
-
-
